afff521e61ee166c22b0c7d619f02f1dd420e627
rabia-tech-ops <ozairrabia@gmail.com>
2026-05-18 10:39:44 +0500

    Implement NavigationSystem class with route setting

---
+++ navigation.cpp
@@ -0,0 +1,17 @@
+#include "NavigationSystem.h"
+
+// Constructor (ONLY THIS HERE)
+
+NavigationSystem::NavigationSystem(string src, string dest) {
+
+    source = src;
+    destination = dest;
+}
+
+// Set route function
+
+void NavigationSystem::setRoute(string src, string dest) {
+
+    source = src;
+    destination = dest;
+}
